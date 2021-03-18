4a5d2dc4b9643ffc7f5ad1ff0c88923b3a02050f
Joe <47789741+jcfoxtrot54@users.noreply.github.com>
2021-03-18 21:21:09 +0000

    v3 update

--- status.py
+++ status.py
@@ -1,95 +1,94 @@
-ServerAddress="http://localhost" # <==== CHANGE THIS TO THE IP OF YOUR SERVER IF IT'S NOT HOSTED ON THE SAME MACHINE (INCLUDE NO PORTS)
-ChannelID=0 			 # <==== CHANNEL ID YOU WANT STATUS OUPUT
-BotToken="" 			 # <==== BOT TOKEN 
+#######################################
+#######################################
+################ ABOUT ################
+#######################################
+#######################################
 
-import discord # import discord library
-import requests # import requests library
-import json # import json library
-import datetime # import datetime library
-from time import sleep # import sleep function from time library
+# GITHUB REPO: https://github.com/jcfoxtrot54/FiveM-Discord-Bot
+# AUTHOR: JCF#7536
+# NOTE: edit this to your heart's content, it's less than 100 lines, if you're generous plz give credit xoxo
+# VERSION: 3
 
-ChannelObject=None # store object of channel
-ActiveMessageObject=None # store object of status message
+#######################################
+#######################################
+############ CONFIGURATION ############
+#######################################
+#######################################
 
+ChannelID=709801509210030160 # CHANNEL ID YOU WANT STATUS OUPUT
+Servers=[ # LIST OF SERVERS TO DISPLAY // NOTE: IF BYPASS IS SET TO TRUE IT WILL ACT AS A LINE BREAK
+	{"URL": "http://localhost:30120", "DisplayName":"Los Santos #1", "Bypass":False},
+	{"URL": "http://localhost:30122", "DisplayName":"Los Santos #2", "Bypass":False},
+	{"Bypass":True},
+	{"URL": "http://localhost:30124", "DisplayName":"Los Santos #3", "Bypass":False},
+	{"URL": "http://localhost:30126", "DisplayName":"Los Santos #4", "Bypass":False},
+	{"Bypass":True},
+	{"Bypass":True},
+	{"URL": "http://localhost:30121", "DisplayName":"Tracks #1", "Bypass":False},
+	{"URL": "http://localhost:30123", "DisplayName":"Tracks #2", "Bypass":False},
+	{"Bypass":True},
+	{"URL": "http://localhost:30125", "DisplayName":"Tracks #3", "Bypass":False},
+	{"URL": "http://localhost:30127", "DisplayName":"Tracks #4", "Bypass":False},
+	{"Bypass":True},
+	{"Bypass":True},
+	{"URL": "http://localhost:30130", "DisplayName":"Underground #1", "Bypass":False},
+	{"URL": "http://localhost:30131", "DisplayName":"Underground #2", "Bypass":False},
+]
+EmbedTitle="Velocity Networks FiveM Server Status (https://github.com/jcfoxtrot54/FiveM-Discord-Bot)" # TITLE OF THE EMBED
+UpdateDelay=5 # DELAY FOR STATUS TO UPDATE (SECONDS)
+CounterCap=30 # HOW MANY TIMES THE MESSAGE SHOULD UPDATE BEFORE IT IS REGENERATED
+PublicIP="velocitynetworks.org" # PUBLIC IP TO DISPLAY
+BotToken="lmao no" # BOT TOKEN 
+
+#######################################
+#######################################
+######## DON'T EDIT BELOW HERE ########
+#######################################
+#######################################
+
+try:
+	import discord # import discord library
+	import requests # import requests library
+	import json # import json library
+	import datetime # import datetime library
+	from time import sleep # import sleep function from time library
+except ImportError as module: # if an import error is raised
+	print("ERROR: CANNOT IMPORT '"+module.name+"' LIBRARY. IS IT INSTALLED?")
+	exit() # halt execution
 
 class MyClient(discord.Client):
-	async def on_ready(self):
+	def __init__(self, CounterCap, Servers, ChannelID, EmbedTitle, Delay, PublicIP):
+		super().__init__() # inherit parent class' attributes (discord.Client)
+		self.ChannelObject=None
+		self.ActiveMessageObject=None
+		self.Counter=50
+		self.CounterCap=CounterCap
+		self.Servers=Servers
+		self.ChannelID=ChannelID
+		self.EmbedTitle=EmbedTitle
+		self.Delay=Delay
+		self.PublicIP=PublicIP
+
+	async def on_ready(self): # when bot is ready
 		print('Connected as {0}!'.format(self.user)) # identify self in conssole
-		ChannelObject=await self.fetch_channel(ChannelID) # get channel from id specified
-		MessageInvalid=True
+		self.ChannelObject=await self.fetch_channel(self.ChannelID) # get channel from id specified
 		while True:
-			if MessageInvalid: # if the message object is invalid, designate a new one
-				await ChannelObject.purge() # clear channel
-				ActiveMessageObject=await ChannelObject.send("Loading...") # add new message
-				MessageInvalid=False # set mesagevalidity to valid
-			else: # if message is valid
-				try:
-					statusLS1=":green_circle: "+str(len(requests.get(ServerAddress+":30120/players.json", timeout=2, verify=False).json()))+"/32 players"+" <:pepecool:666781477224054814>"+"\n\nFind me on the FiveM server listings!\nDirect connect via: `54.37.245.85:30120`"
-				except:
-					statusLS1=":red_circle: No response <:pepesad:666781477236768768>\n\n I'm probably just offline"
-				try:
-					statusLS2=":green_circle: "+str(len(requests.get(ServerAddress+":30122/players.json", timeout=2, verify=False).json()))+"/32 players"+" <:pepecool:666781477224054814>"+"\n\nFind me on the FiveM server listings!\nDirect connect via: `54.37.245.85:30122`"
-				except:
-					statusLS2=":red_circle: No response <:pepesad:666781477236768768>\n\n I'm probably just offline"
-				try:
-					statusLS3=":green_circle: "+str(len(requests.get(ServerAddress+":30124/players.json", timeout=2, verify=False).json()))+"/32 players"+" <:pepecool:666781477224054814>"+"\n\nFind me on the FiveM server listings!\nDirect connect via: `54.37.245.85:30124`"
-				except:
-					statusLS3=":red_circle: No response <:pepesad:666781477236768768>\n\n I'm probably just offline"
-				try:
-					statusLS4=":green_circle: "+str(len(requests.get(ServerAddress+":30126/players.json", timeout=2, verify=False).json()))+"/32 players"+" <:pepecool:666781477224054814>"+"\n\nFind me on the FiveM server listings!\nDirect connect via: `54.37.245.85:30126`"
-				except:
-					statusLS4=":red_circle: No response <:pepesad:666781477236768768>\n\n I'm probably just offline"
-
-				try:
-					statusTR1=":green_circle: "+str(len(requests.get(ServerAddress+":30121/players.json", timeout=2, verify=False).json()))+"/32 players"+" <:pepecool:666781477224054814>"+"\n\nFind me on the FiveM server listings!\nDirect connect via: `54.37.245.85:30121`"
-				except:
-					statusTR1=":red_circle: No response <:pepesad:666781477236768768>\n\n I'm probably just offline"
-				try:
-					statusTR2=":green_circle: "+str(len(requests.get(ServerAddress+":30123/players.json", timeout=2, verify=False).json()))+"/32 players"+" <:pepecool:666781477224054814>"+"\n\nFind me on the FiveM server listings!\nDirect connect via: `54.37.245.85:30123`"
-				except:
-					statusTR2=":red_circle: No response <:pepesad:666781477236768768>\n\n I'm probably just offline"
-				try:
-					statusTR3=":green_circle: "+str(len(requests.get(ServerAddress+":30125/players.json", timeout=2, verify=False).json()))+"/32 players"+" <:pepecool:666781477224054814>"+"\n\nFind me on the FiveM server listings!\nDirect connect via: `54.37.245.85:30125`"
-				except:
-					statusTR3=":red_circle: No response <:pepesad:666781477236768768>\n\n I'm probably just offline"
-				try:
-					statusTR4=":green_circle: "+str(len(requests.get(ServerAddress+":30127/players.json", timeout=2, verify=False).json()))+"/32 players"+" <:pepecool:666781477224054814>"+"\n\nFind me on the FiveM server listings!\nDirect connect via: `54.37.245.85:30127`"
-				except:
-					statusTR4=":red_circle: No response <:pepesad:666781477236768768>\n\n I'm probably just offline"
-
-				try:
-					statusUG1=":green_circle: "+str(len(requests.get(ServerAddress+":30130/players.json", timeout=2, verify=False).json()))+"/32 players"+" <:pepecool:666781477224054814>"+"\n\nFind me on the FiveM server listings!\nDirect connect via: `54.37.245.85:30130`"
-				except:
-					statusUG1=":red_circle: No response <:pepesad:666781477236768768>\n\n I'm probably just offline"
-				try:
-					statusUG2=":green_circle: "+str(len(requests.get(ServerAddress+":30131/players.json", timeout=2, verify=False).json()))+"/32 players"+" <:pepecool:666781477224054814>"+"\n\nFind me on the FiveM server listings!\nDirect connect via: `54.37.245.85:30131`"
-				except:
-					statusUG2=":red_circle: No response <:pepesad:666781477236768768>\n\n I'm probably just offline"
-				# create embed
-				StatusEmbed=discord.Embed(title="Velocity Networks Live Status", description="**This status was last updated: "+datetime.datetime.now().strftime("%H:%M:%S %d/%m/%y %Z")+"**\nTo join one of our servers, find us on the FiveM server listings!")
-				StatusEmbed.add_field(name="\u200b", value="\u200b", inline=False)
-				StatusEmbed.add_field(name="Los Santos #1", value=str(statusLS1), inline=True)
-				StatusEmbed.add_field(name="Los Santos #2", value=str(statusLS2), inline=True)
-				StatusEmbed.add_field(name="\u200b", value="\u200b", inline=False)
-				StatusEmbed.add_field(name="Los Santos #3 (handling editor)", value=str(statusLS3), inline=True)
-				StatusEmbed.add_field(name="Los Santos #4 (handling editor)", value=str(statusLS4), inline=True)
-				StatusEmbed.add_field(name="\u200b", value="\u200b", inline=False)
-				StatusEmbed.add_field(name="\u200b", value="\u200b", inline=False)
-				StatusEmbed.add_field(name="Tracks #1", value=str(statusTR1), inline=True)
-				StatusEmbed.add_field(name="Tracks #2", value=str(statusTR2), inline=True)
-				StatusEmbed.add_field(name="\u200b", value="\u200b", inline=False)
-				StatusEmbed.add_field(name="Tracks #3 (handling editor)", value=str(statusTR3), inline=True)
-				StatusEmbed.add_field(name="Tracks #4 (handling editor)", value=str(statusTR4), inline=True)
-				StatusEmbed.add_field(name="\u200b", value="\u200b", inline=False)
-				StatusEmbed.add_field(name="\u200b", value="\u200b", inline=False)
-				StatusEmbed.add_field(name="Underground #1", value=str(statusUG1), inline=True)
-				StatusEmbed.add_field(name="Underground #2", value=str(statusUG2), inline=True)
-
-				try: # attempt to
-					await ActiveMessageObject.edit(content=None, embed=StatusEmbed) # edit the message object
-				except: # if it fails
-					print("Message object invalid. Regenerating")
-					MessageInvalid=True # mark object as invalid to be reassigned
-			sleep(10) # wait 10 seconds
-client=MyClient()
-client.run(BotToken)
+			if self.Counter>=self.CounterCap: # if counter is not greater than or equal to defined limit
+				await self.ChannelObject.purge() # purge channel
+				self.ActiveMessageObject=await self.ChannelObject.send("Loading...") # add new message
+				self.Counter=0 # reset counter
+			StatusEmbed=discord.Embed(title=self.EmbedTitle, description="**This status was last updated: "+datetime.datetime.now().strftime("%H:%M:%S %d/%m/%y %Z")+"**\nTo join one of our servers, find us on the FiveM server listings!") # date time pulls HOUR:MINUTE:SECOND DAY:MONTH:YEAR TIMEZONE (so you know if bot's crashed)
+			for serverObj in self.Servers: # for each dictionary in Servers object
+				if serverObj["Bypass"]: # if bypass is set to True
+					StatusEmbed.add_field(name="\u200b", value="\u200b", inline=False) # insert blanking object
+				else:
+					try:
+						StatusEmbed.add_field(name=serverObj["DisplayName"], value=":green_circle: "+str(len(requests.get(serverObj["URL"]+"/players.json", timeout=2, verify=False).json()))+"/32 players"+" <:pepecool:666781477224054814>"+"\n\nFind me on the FiveM server listings!\nDirect connect via: `"+PublicIP+":"+serverObj["URL"][-5:]+"`", inline=True)
+						# runs HTTP request to specified endpoint with a timeout of 2 seconds, encodes it into JSON then gets the length of it (to get number of players). Verify is false because who needs SSL anyway :shrug:
+					except: # if this errors (endpoint isn't reachable)
+						StatusEmbed.add_field(name=serverObj["DisplayName"], value=":red_circle: No response <:pepesad:666781477236768768>\n\n I'm probably just offline", inline=True)
+			await self.ActiveMessageObject.edit(content=None, embed=StatusEmbed) # edit the message object
+			self.Counter+=1 # incremement counter by one
+			sleep(Delay) # wait for specified delay in seconds
+client=MyClient(CounterCap, Servers, ChannelID, EmbedTitle, UpdateDelay, PublicIP) # instanciate client from class
+client.run(BotToken) # run client with specified bot token
